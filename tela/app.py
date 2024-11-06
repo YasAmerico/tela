481b4b91bfe309e399c33935a62cfbb2bca1f4dc
YasAmerico <166563849+YasAmerico@users.noreply.github.com>
2024-11-06 15:02:45 -0300

    Update app.py

--- tela/app.py
+++ tela/app.py
@@ -2,4 +2,20 @@ from fastapi import FastAPI
 from ariadne import QueryType,make_executable_schema,graphql
 from ariadne.asgi import GraphQl
 
+#defina o esquema graphQL
+type_defs="""
+    type Query {
+        hello:String!
+    }
+"""
+query = QueryType()
 
+@query.field("hello")
+async def resolve_hello(_,info):
+    return "Olá,mundo!"
+
+schema =make_executable_schema(type_defs,query)
+
+app = FastAPI()
+
+app.add route("/graphql"GraphQl(schema,debug=True))
